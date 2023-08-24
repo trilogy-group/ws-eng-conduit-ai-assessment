ff96bec997e5f72e4e3dbfa116eb4d506979d1f4
Serban Petrescu <Serban.Petrescu@outlook.com>
2023-08-24 17:54:45 +0300

    Merges the two apps together, enhances README.

---
+++ apps/frontend/src/app/layout/navbar/navbar.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { NavbarComponent } from './navbar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [NavbarComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
