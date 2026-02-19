447f8702666b79cf435463f1796690bda472274e
rameshsalapathi08 <rameshsalapathi08@gmail.com>
2026-02-19 12:17:39 +0530

    Update NewArticle.tsx

--- frontend/src/components/Pages/NewArticle/NewArticle.tsx
+++ frontend/src/components/Pages/NewArticle/NewArticle.tsx
@@ -2,20 +2,28 @@ import { FormEvent, useEffect } from 'react';
 import { createArticle } from '../../../services/conduit';
 import { store } from '../../../state/store';
 import { ArticleEditor } from '../../ArticleEditor/ArticleEditor';
-import { initializeEditor, startSubmitting, updateErrors } from '../../ArticleEditor/ArticleEditor.slice';
+import {
+  initializeEditor,
+  startSubmitting,
+  updateErrors,
+} from '../../ArticleEditor/ArticleEditor.slice';
 
 export function NewArticle() {
   useEffect(() => {
     store.dispatch(initializeEditor());
-  }, [null]);
+  }, []);
 
   return <ArticleEditor onSubmit={onSubmit} />;
 }
 
 async function onSubmit(ev: FormEvent) {
   ev.preventDefault();
+
   store.dispatch(startSubmitting());
-  const result = await createArticle(store.getState().editor.article);
+
+  const articleData = store.getState().editor.article;
+
+  const result = await createArticle(articleData);
 
   result.match({
     err: (errors) => store.dispatch(updateErrors(errors)),
