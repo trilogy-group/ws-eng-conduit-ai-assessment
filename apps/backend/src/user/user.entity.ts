1599deced60fc5273276286c92b09c2af7416e4f
Serban Petrescu <serban.petrescu@trilogy.com>
2026-08-18 13:28:16 +0300

    chore(conduit): Replaces broken upstream CSS/image link.

--- apps/backend/src/user/user.entity.ts
+++ apps/backend/src/user/user.entity.ts
@@ -65,7 +65,7 @@ export class User {
 
   toJSON(user?: User) {
     const o = wrap<User>(this).toObject() as UserDTO;
-    o.image = this.image || 'https://static.productionready.io/images/smiley-cyrus.jpg';
+    o.image = this.image || 'https://api.dicebear.com/9.x/initials/svg?seed=U';
     o.following = user && user.followers.isInitialized() ? user.followers.contains(this) : false; // TODO or followed?
 
     return o;
